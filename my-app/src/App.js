a4bbc68988157d05f009d65a0f80e9e31d3f8efb
akashramkar <akashrramkar2001@gmail.com>
2022-10-31 13:49:31 +0530

    image asset added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
+import Card from "./application_components/card.js";
 
 function App() {
-	return <>welcome to the project</>;
+	return (
+		<>
+			welcome to the project
+			<Card />
+		</>
+	);
 }
 
 export default App;

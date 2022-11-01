6e6c5ff0f4bebb75870380221ecf5549a3482444
bhavik919 <bhavikbedmutha919@gmail.com>
2022-11-01 15:49:28 +0530

    login pag created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,20 @@
-//import logo from "./logo.svg";
-import "./App.css";
+
+import "./index.css";
 import Card from "./application_components/card.js";
+import Login from "./application_components/login";
 
 function App() {
 	return (
 		<>
-			welcome to the project
-			<Card />
+			welcome to the project 
+			 <Card/>
+			<div className="App">
+             <Login/>
+			</div>
+           
+		
 		</>
+	
 	);
 }
 

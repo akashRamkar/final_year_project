4b5dacd8c2a329711e7bafe59597e096b8c2b1ef
yashgaikwad <yashrgaikwad1235@gmail.com>
2022-11-01 16:50:53 +0530

    fixed merge issue

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import Topic_Title from "./application_components/Topic_Title";
 
 
-function App() {
-	return <>
-	
-	<Topic_Title/>
-	
-	{/* //<Footer/> */}
 
-	</>;
-=======
-// import Card from "./application_components/card.js";
+import "./App.css";
 import Thumbnail from "./imageAssets/pdfImage.png";
 import Cards from "./application_components/cards";
 
@@ -21,11 +13,9 @@ function App() {
 	return (
 		<>
 			welcome to the project
-			{/* <Card thumbnail={Thumbnail} /> */}
 			<Cards thumbnail={Thumbnail} />
+			<Topic_Title/>
 		</>
 	);
->>>>>>> 928c6397bf8dd3f721fd60b83e3a366b7af59034
 }
-
 export default App;

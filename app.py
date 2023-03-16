839962d2b97186e06372cb5669b955f4ebfcdeac
akashramkar <akashrramkar2001@gmail.com>
2023-03-16 18:48:30 +0530

    python model added

---
+++ app.py
@@ -0,0 +1,16 @@
+# from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     # Get data from the request
+#     data = request.json
+
+#     # Process the data using your pre-trained model
+#     result = your_pretrained_model.predict(data)
+
+#     # Return the result as JSON
+#     return jsonify({'result': result})
+# if __name__ == '__main__':
+#     app.run()
---
+++ key.js
@@ -0,0 +1,2 @@
+const model_pass = "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";
+export default model_pass;
